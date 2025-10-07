6a5c3a5413deef06681b449c31e68b5cb94d6cf6
Lexical Logic <tabulamantis@gmail.com>
2025-10-07 16:52:20 -0400

    chore: loosen vite allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080,
-    allowedHosts: 'all'  // Permite todos los hosts (menos seguro pero funciona)
+    allowedHosts: 'all'
   }
 })

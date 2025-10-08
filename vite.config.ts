7a3b4b764edd07c0642574ca65a12780a55c587c
Lexical Logic <tabulamantis@gmail.com>
2025-10-07 21:08:53 -0400

    Fix vite config and resolve import issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: 'all'
+    port: 8080
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
